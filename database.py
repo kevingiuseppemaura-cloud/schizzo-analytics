fbc295e611dbc8e4c5e541c02cdef83d53dc1a2a
kevingiuseppemaura-cloud <kevingiuseppemaura@gmail.com>
2026-09-19 11:22:12 +0200

    Add files via upload

--- database.py
+++ database.py
@@ -62,6 +62,8 @@ DEFAULT_ALLENATORE = {"allenatore": "Non dichiarato", "indice_tattico": 5}
 DEFAULT_LAMBDA = {"lambda_casa": 1.65, "lambda_ospite": 1.20}
 
 DB_STADI = {
+    "default": DEFAULT_STADIO,
+    
     # SERIE A
     "atalanta": {"stadio": "Gewiss Stadium", "citta": "Bergamo", "campo": "erba_naturale", "lat": 45.71, "lon": 9.68, "media_cartellini": 2.4, "coperto": False},
     "bologna": {"stadio": "Stadio Renato Dall'Ara", "citta": "Bologna", "campo": "erba_naturale", "lat": 44.49, "lon": 11.31, "media_cartellini": 2.3, "coperto": False},
